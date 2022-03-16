4f77e6d357b68550d10d3eb1ce9c797bdb98a846
HawaaEva <awaseye@esp.sn>
2022-03-16 21:46:00 +0000

    modification version java

--- src/main/java/io/work/MapJeunesse/Controller/AuthController.java
+++ src/main/java/io/work/MapJeunesse/Controller/AuthController.java
@@ -5,10 +5,7 @@ import io.work.MapJeunesse.security.request.LoginRequest;
 import io.work.MapJeunesse.security.request.SignupRequest;
 import io.work.MapJeunesse.services.AuthService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
--- src/main/java/io/work/MapJeunesse/Controller/CandidatController.java
+++ src/main/java/io/work/MapJeunesse/Controller/CandidatController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/candidat")
+
 public class CandidatController {
     @Autowired
     private CandidatService candidatService;
